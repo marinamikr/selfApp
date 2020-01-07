29e57150882f44d5433284ec49c894da01c39311
Marina <32869115+marinamikr@users.noreply.github.com>
2020-01-07 20:39:04 +0900

    1/7.2

---
+++ self/RouletteViewController.swift
@@ -0,0 +1,52 @@
+//
+//  RouletteViewController.swift
+//  self
+//
+//  Created by 原田摩利奈 on 2020/01/07.
+//  Copyright © 2020 原田摩利奈. All rights reserved.
+//
+
+import UIKit
+
+class RouletteViewController: UIViewController {
+    
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var label2: UILabel!
+    @IBOutlet var label3: UILabel!
+    @IBOutlet var label4: UILabel!
+    
+    @IBOutlet var image1: UIImageView!
+    @IBOutlet var image2: UIImageView!
+    @IBOutlet var image3: UIImageView!
+    @IBOutlet var image4: UIImageView!
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func againButton(){
+        
+    }
+    
+    @IBAction func saveButton() {
+        
+    }
+    @IBAction func startButton(){
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- self/SelectCharacterViewController.swift
+++ self/SelectCharacterViewController.swift
@@ -12,39 +12,60 @@ import RealmSwift
 class SelectCharacterViewController: UIViewController {
     
     var realm :Realm!
-
+    
     
     @IBOutlet var nameLabel:UILabel!
     @IBOutlet var textField1: UITextField!
     @IBOutlet var textField2: UITextField!
     @IBOutlet var textField3: UITextField!
+    @IBOutlet var textField4: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         realm = try! Realm()
-
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func saveButton() {
-        let saveData = RealmModel()
-              saveData.text = textField1.text
-              try! realm.write() {
-                  realm.add(saveData)
-              }
-              print(saveData.text)
-              
+        let saveData1 = RealmModel()
+        saveData1.text = textField1.text
+        try! realm.write() {
+            realm.add(saveData1)
+        }
+        
+        let saveData2 = RealmModel()
+        saveData2.text = textField2.text
+        try! realm.write() {
+            realm.add(saveData2)
+        }
+        let saveData3 = RealmModel()
+        saveData3.text = textField3.text
+        try! realm.write() {
+            realm.add(saveData3)
+        }
+        let saveData4 = RealmModel()
+        saveData4.text = textField4.text
+        try! realm.write() {
+            realm.add(saveData4)
+        }
+        
+        print(saveData1.text)
+        print(saveData2.text)
+        print(saveData3.text)
+
+        
     }
     
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
